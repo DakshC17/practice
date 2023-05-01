3f3a6a83869e4b4224593d76f5bfa26dbbe71609
DakshC17 <132166506+DakshC17@users.noreply.github.com>
2023-05-01 19:42:31 +0530

    Create nesif.cpp

---
+++ assignment1/nesif.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{int n;
+cout<<"enter any number:";
+cin>>n;
+if(n%2==0)
+{cout<<"EVEN";
+if(n%2!=0)
+{cout<<"ODD";
+if(n==0)
+cout<<"Zero";
+}}
+return 0;
+}
