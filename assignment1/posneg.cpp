f9ad1ce6bb82bd3b1105f90cdb5bb7612a507d66
DakshC17 <132166506+DakshC17@users.noreply.github.com>
2023-05-01 19:33:37 +0530

    Create posneg.cpp

---
+++ assignment1/posneg.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{int number;
+cout<<"Please enter any number:";
+cin>>number;
+if(number>0)
+cout<<"Positive";
+else
+cout<<"Negative";
+return 0;
+}
